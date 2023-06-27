92e933e27aeedc6167e50936ed3b0e8a35ede371
VladislavPerov <vladislav.peroff2016@yandex.ru>
2023-06-27 17:00:55 +0700

    refactoring Main.kt Main.kt

--- src/test/kotlin/CompareTester.kt
+++ src/test/kotlin/CompareTester.kt
@@ -3,16 +3,13 @@ import org.junit.jupiter.api.RepetitionInfo
 import kotlin.test.assertEquals
 
 class CompareTester {
-
     @RepeatedTest(value = 1000)
     fun unitTest(repetitionInfo: RepetitionInfo){
-        val s1 = RandomMy(10,false,true).uppercase()
-        val s2 = RandomMy(10,false,true).uppercase()
+        val s1 = RandomMy(10, f = false, k = true).uppercase()
+        val s2 = RandomMy(10, f = false, k = true).uppercase()
         val actual = CompareMy(s1,s2)
         val excepted = s1.compareTo(s2)
-
         assertEquals(actual, excepted, "Тест ${repetitionInfo.currentRepetition} упал. Ожидаемый результат: $excepted, Фактический результат: $actual")
-
         if(actual==excepted){
             println("----test ${repetitionInfo.currentRepetition} is successful")
         }
@@ -20,5 +17,4 @@ class CompareTester {
             println("----test ${repetitionInfo.currentRepetition} invalid")
         }
     }
-
 }
